054a147f14f463229b7ca5a8c793e523687d8b5d
Pliskin <arieljanickcarvalho@gmail.com>
2024-08-14 17:54:26 +0100

    First API with springboot

---
+++ src/main/java/io/arielcarvalho/Main.java
@@ -0,0 +1,22 @@
+package io.arielcarvalho;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+// We need the annotation and the spring application run to say this is a spring application
+
+@SpringBootApplication
+@RestController
+public class Main {
+
+   public static void main(String[] args) {
+       SpringApplication.run(Main.class, args);
+   }
+
+   @GetMapping("/greet")
+   public String greet() {
+       return "Hello there!";
+   }
+}
