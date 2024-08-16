5d4e0cae842350dbc6166b3066888b3c1c56dd30
Pliskin <arieljanickcarvalho@gmail.com>
2024-08-16 17:29:48 +0100

    Set up connection to docker container and postgress database

---
+++ src/main/java/io/arielcarvalho/Customer.java
@@ -0,0 +1,61 @@
+package io.arielcarvalho;
+
+public class Customer {
+    private Integer id;
+    private String name;
+    private String email;
+    private Integer age;
+
+    public Customer(Integer id, String name, String email, Integer age) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.age = age;
+    }
+
+    // A no args constructor
+
+    public Customer() {
+        
+    }
+
+
+    // Getters
+
+    public Integer getId() {
+        return  this.id;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public String getEmail() {
+        return this.email;
+    }
+
+    public Integer getAge() {
+        return this.age;
+    }
+
+    // id, name, email, age
+    // Setters
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+
+}
