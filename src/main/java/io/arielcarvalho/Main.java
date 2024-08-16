8741fa6c44467745dd47d3c9d54c72d0de1b1563
Pliskin <arieljanickcarvalho@gmail.com>
2024-08-16 21:06:27 +0100

    Stopping point

--- src/main/java/io/arielcarvalho/Main.java
+++ src/main/java/io/arielcarvalho/Main.java
@@ -2,12 +2,15 @@ package io.arielcarvalho;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 // We need the annotation and the spring application run to say this is a spring application
 
-@SpringBootApplication
+// @SpringBootApplication
+// This or there will be error with starting the app (because of the db)
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
 @RestController
 public class Main {
 
